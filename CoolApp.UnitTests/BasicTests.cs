73e9120cdc3847fbee87595a23de72f6977142f8
niku-live <nku2007@gmail.com>
2025-12-04 15:51:56 +0200

    Refactor to separate projects/folders + Added unit tests project

---
+++ CoolApp.UnitTests/BasicTests.cs
@@ -0,0 +1,21 @@
+﻿namespace CoolApp.UnitTests;
+
+[TestClass]
+public sealed class BasicTests
+{
+    [TestMethod]
+    public void Test_TemperatureCalculation()
+    {
+        // Arrange
+        var forecast = new CoolApp.Models.WeatherForecast
+        {
+            TemperatureC = 100
+        };
+
+        // Act
+        var temperatureF = forecast.TemperatureF;
+
+        // Assert
+        Assert.AreEqual(211, temperatureF);
+    }
+}
